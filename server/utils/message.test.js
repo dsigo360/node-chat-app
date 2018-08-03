d2d6de78f23455867dd44f0919cca936fb1c6f7b
dsigo360 <dsigo360@me.com>
2018-08-03 13:40:59 -0500

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 const expect = require('expect');
 
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -11,5 +11,18 @@ describe('generateMessage', () => {
 
     expect(typeof message.createdAt).toBe('number');
     expect(message).toMatchObject({ from, text });
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Ducky2';
+    const latitude = 37;
+    const longitude = -122;
+    const url = 'http://google.com/maps?q=37,-122';
+    const message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({ from, url });
   });
 });
